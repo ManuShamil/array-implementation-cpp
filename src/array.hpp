3fe866f27b23f2e112730523c1b6a761e4684392
Manu Shamil <42031273+ManuShamil@users.noreply.github.com>
2021-06-13 18:04:57 +0530

    Remove, Count
    Added functions for count and remove

--- src/array.hpp
+++ src/array.hpp
@@ -1,23 +1,41 @@
-
+#include <stdio.h>
+#include <iostream>
 
 template<typename T>
 class Element 
 {
     T myValue;
     Element<T> *next;
+    Element<T> *previous;
     public:
+
         Element(T a)
         {
             myValue = a;
+            next = NULL;
+            previous = NULL;
         }
+
         void SetNextElement(Element<T> *a)
         {
             next = a;
         }
+
+        void SetPreviousElement(Element<T> *a)
+        {
+            previous = a;
+        }
+
         Element<T>* GetNextElement()
         {
             return next;
         }
+
+        Element<T>* GetPreviousElement()
+        {
+            return previous;
+        }
+
         T GetValue()
         {
             return myValue;
@@ -45,7 +63,10 @@ class Array
                 firstElement = newElement;
 
             if (lastElement != NULL)
+            {
                 lastElement -> SetNextElement( newElement );
+                newElement -> SetPreviousElement( lastElement );
+            }
 
             lastElement = newElement;
 
@@ -64,5 +85,65 @@ class Array
             }
 
             return cursor -> GetValue();
+        }
+
+        bool Remove( int index )
+        {
+            Element<T> *cursor = firstElement;
+
+            int i=0;
+            while( i < index)
+            {
+                cursor = cursor->GetNextElement();
+                i++;
+            }
+
+            if (cursor == NULL)
+                return false;
+
+            Element<T> * prevElement = cursor->GetPreviousElement();
+            Element<T> * nextElement = cursor->GetNextElement();
+
+            if(prevElement)
+                prevElement->SetNextElement( cursor->GetNextElement() );
+            else
+                firstElement = cursor->GetNextElement();
+            
+            if(nextElement)
+                nextElement->SetPreviousElement(cursor -> GetPreviousElement());
+
+            return true;
+        }
+
+        int Count()
+        {
+            int i=0;
+
+            Element<T> *cursor = firstElement;
+
+            while( cursor != NULL)
+            {
+                cursor = cursor->GetNextElement();
+                i++;
+            }
+
+            return i;
+        }
+
+        void DebugDisplay()
+        {
+            Element<T> *cursor = firstElement;
+
+            printf("\n------- DEBUG ARRAY --------\n");
+            printf("Elements Count: %d \n", Count() );
+            while( cursor != NULL)
+            {
+                std::cout << " -> "<< cursor->GetValue();
+
+                cursor = cursor->GetNextElement();
+
+            }
+
+            std::cout << std::endl;
         }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,13 @@ int main()
     std::Array<int> *m_Array = new Array<int>;
     m_Array -> Insert(5);
     m_Array -> Insert(10);
+    m_Array -> Insert(6);
 
-    printf( "%d", m_Array->Get(1) );
+    m_Array->DebugDisplay();
+
+    m_Array->Remove(2);
+
+    m_Array->DebugDisplay();
 
     return 1;
 }
